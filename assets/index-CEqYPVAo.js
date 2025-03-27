8e972acfea4092b3b3369d5927bb58e0e79ab057
Ghz-git <928580227@qq.com>
2025-03-27 11:16:34 +0800

    update

--- assets/index-CEqYPVAo.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./loading-BWgQsHV-.js";import{g as r,o as a}from"./index-DlFSwqvT.js";const s={__name:"index",setup(e){return(t,_)=>(a(),r(o,{duration:4200}))}};export{s as default};
