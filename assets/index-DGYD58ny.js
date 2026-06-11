6944d9ca05f7c5b6b7ef29b37a9b8a2fc2128a19
Ghz-git <928580227@qq.com>
2026-06-11 11:54:53 +0800

    feat:添加PWA

---
+++ assets/index-DGYD58ny.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./loading-D2eg9efK.js";import{i as r,o as a}from"./index-BKfqix61.js";const s={__name:"index",setup(e){return(t,_)=>(a(),r(o,{duration:4200}))}};export{s as default};
